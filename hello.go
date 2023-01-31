9ab8956f8adaffbbd72da80cca5d10e6c3420213
Celso Benedetti <celso.benedetti@dell.com>
2023-01-31 16:22:46 -0300

    new: refactored tests into subtests and mapped language prefixes

--- hello.go
+++ hello.go
@@ -4,8 +4,28 @@ import (
 	"fmt"
 )
 
-func Hello(name string) string {
-	return "Hello, " + name + "!"
+var helloPrefixes = map[string]string{
+	"en": "Hello",
+	"sp": "Hola",
+	"fr": "Bonjour",
+}
+
+func Hello(params ...string) string {
+	var name, lang string
+
+	if len(params) > 0 && len(params[0]) > 0 {
+		name = params[0]
+	} else {
+		name = "World"
+	}
+
+	if len(params) > 1 {
+		lang = params[1]
+	} else {
+		lang = "en"
+	}
+
+	return helloPrefixes[lang] + ", " + name + "!"
 }
 
 func main() {
--- hello_test.go
+++ hello_test.go
@@ -4,21 +4,42 @@ import (
 	"testing"
 )
 
-func TestHelloWorld(t *testing.T) {
-	got := Hello("World")
-	want := "Hello, World!"
+func TestHello(t *testing.T) {
+	t.Run("saying hello to Celso", func(t *testing.T) {
+		got := Hello("Celso")
+		want := "Hello, Celso!"
+
+		assertCorrectMesssage(t, got, want)
+	})
+
+	t.Run("saying hello with empty string", func(t *testing.T) {
+		got := Hello()
+		want := "Hello, World!"
+
+		assertCorrectMesssage(t, got, want)
+	})
+
+	t.Run("saying Hello to Jose in spanish", func(t *testing.T) {
+		got := Hello("Jose", "sp")
+		want := "Hola, Jose!"
+
+		assertCorrectMesssage(t, got, want)
+	})
+
+	t.Run("saying Hello to Pierre in french", func(t *testing.T) {
+		got := Hello("Pierre", "fr")
+		want := "Bonjour, Pierre!"
+
+		assertCorrectMesssage(t, got, want)
+	})
+
+}
+
+func assertCorrectMesssage(t testing.TB, got, want string) {
+	//used to tell the test suite that this method is a helper. By doing this when it fails the line number reported will be in our function call rather than inside our test helper.
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
-
-func TestHelloYou(t *testing.T) {
-	got := Hello("Celso")
-	want := "Hello, Celso!"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
-}
-
