72faf4e0f574b374a667cbdbc08bb2282e1dd3fe
Celso Benedetti <celso.benedetti@dell.com>
2023-02-07 20:11:29 -0300

    new: reflection tdt with mustiple cases

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -7,31 +7,44 @@ import "reflect"
 func Walk(x any, fn func(string)) {
 	val := getValue(x)
 
-    if val.Kind() == reflect.Slice {
-        for i := 0; i < val.Len(); i++ {
-            Walk(val.Index(i).Interface(), fn)
-        }
-        return
-    }
+	walkValue := func(value reflect.Value) {
+		Walk(value.Interface(), fn)
+	}
 
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
 
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			Walk(field.Interface(), fn)
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	case reflect.Chan:
+		for v, ok := val.Recv(); ok; v, ok = val.Recv() {
+			walkValue(v)
+		}
+	case reflect.Func:
+		valFnResult := val.Call(nil)
+		for _, res := range valFnResult {
+			walkValue(res)
 		}
 	}
 }
 
 func getValue(x any) reflect.Value {
-    val := reflect.ValueOf(x)
+	val := reflect.ValueOf(x)
 
 	if val.Kind() == reflect.Ptr {
 		val = val.Elem()
 	}
 
-    return val
+	return val
 }
