a065d41df3c78715c9239f7aa0448605adcdbeb1
Celso Benedetti <celso.benedetti@dell.com>
2023-02-19 09:39:39 -0300

    reset: removed http-server folder
    squash: base PlyerServer and PlayerStore interfaces
    
    remove: http-server

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/celso-patiri/go-tdd/concurrency"
-)
-
-func main() {
-    m := concurrency.ParallelIterate(1000)
-
-    for i := 0; i < 100; i++ {
-        fmt.Printf("Map[%d]: %v\n", i, m[fmt.Sprint(i)])
-    }
-}
-
+// import (
+// 	"fmt"
+//
+// 	"github.com/celso-patiri/go-tdd/concurrency"
+// )
+//
+// func main() {
+//     m := concurrency.ParallelIterate(1000)
+//
+//     for i := 0; i < 100; i++ {
+//         fmt.Printf("Map[%d]: %v\n", i, m[fmt.Sprint(i)])
+//     }
+// }
+//
